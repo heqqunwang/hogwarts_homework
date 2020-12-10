92741d4034c9947218a968d2555c72d316bc4520
whq <hequn@wepie.com>
2020-12-10 23:57:02 +0800

    fix

--- test_cacul.py
+++ test_cacul.py
@@ -8,22 +8,22 @@ class TestCalc:
         self.calc=Calcuator()
         print("结束计算")
 
-    @pytest.mark.parametrize("a,b,expected",[(1,1,3),(2,2,4),(100,1,101)],
+    @pytest.mark.parametrize("a,b,expected",[(1,1,2),(2,2,4),(100,1,101)],
                          ids=["int","minus","bigint"])
     def test_add(self,a,b,expected):
         assert expected==self.calc.add(a,b)
 
-    @pytest.mark.parametrize("a,b,expected", [(1, 1, 0), (2, 2, 0), (100, 1, 100)],
+    @pytest.mark.parametrize("a,b,expected", [(1, 1, 0), (2, 2, 0), (100, 1, 99)],
                              ids=["int", "minus", "bigint"])
     def test_sub(self,a,b,expected):
          assert expected==self.calc.sub(a,b)
 
     @pytest.mark.parametrize("a,b,expected", [(1, 1, 1), (2, 2, 4), (100, 1, 100)],
                              ids=["int", "minus", "bigint"])
-    def test_mutli(self,a,b,expected):
-        assert expected==self.cal.mutli(a,b)
+    def test_multi(self,a,b,expected):
+        assert expected==self.calc.multi(a,b)
 
-    @pytest.mark.parametrize("a,b,expected", [(1, 1, 1), (2, 2, 1), (100, 1, 100)],
+    @pytest.mark.parametrize("a,b,expected", [(1, 1, 1), (2, 2, 1), (100, 1, 10)],
                              ids=["int", "minus", "bigint"])
     def test_div(self,a,b,expected):
         assert expected==self.calc.div(a,b)
